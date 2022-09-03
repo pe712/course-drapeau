f3e18c62f178d67ad66b474a2b21837bc84bef29
pe712 <pe.baviere@gmail.com>
2022-09-03 23:24:56 +0200

    styles update

--- includes/linksAndScripts.php
+++ includes/linksAndScripts.php
@@ -1,9 +1,9 @@
 <link rel="icon" href="img/favicon.jpg">
-<link href="bootstrap-5.2.0-dist\css\bootstrap.css" rel="stylesheet">
+<link href="bootstrap-5.2.0\css\bootstrap.css" rel="stylesheet">
 
 <?php
 if (isset($css))
-    echo "<link rel=stylesheet href=styles/css/$css>";
+    echo "<link rel=stylesheet href=styles/$css>";
 ?>
 
-<script src="bootstrap-5.2.0-dist\js\bootstrap.bundle.js"></script>
+<script src="bootstrap-5.2.0\js\bootstrap.bundle.js"></script>
