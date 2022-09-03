c9cd6f5d0791490f49894417a41f5b9728d3e501
pe712 <pe.baviere@gmail.com>
2022-09-03 21:31:59 +0200

    setting css

--- includes/linksAndScripts.php
+++ includes/linksAndScripts.php
@@ -6,4 +6,4 @@ if (isset($css))
     echo "<link rel=stylesheet href=css/$css>";
 ?>
 
-<script src="bootstrap-5.2.0-dist\js\bootstrap.min.js"></script>
+<script src="bootstrap-5.2.0-dist\js\bootstrap.bundle.js"></script>
