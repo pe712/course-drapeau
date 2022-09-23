f7a1c7ba494c0aec484d09e56ec42d085a177b45
pe712 <pe.baviere@gmail.com>
2022-09-23 15:16:34 +0200

    23.09

--- pages/EspacePerso.php
+++ pages/EspacePerso.php
@@ -10,19 +10,49 @@ if (!is_dir($dossier))
 require("pages/espacePerso/PersoCertifPost.php");
 ?>
 
-<div class="progress">
-  <div class="progress-bar" role="progressbar" style="width: 18%;" aria-valuemin="0" aria-valuemax="100">18%</div>
-</div>
 
-<br>
+<div class="espacePersoMainContainer">
+  <p class="progression">Progression :</p>
 
-<div class="formContainer">
-  <form enctype="multipart/form-data" action="index.php?page=EspacePerso" method="post">
-    <div class="mb-3">
-      <input type="hidden" name="MAX_FILE_SIZE" value="300000" />
-      <label for="certif" class="form-label">Certificat médical de moins de 1 an</label>
-      <input type="file" class="form-control" name="certificat" id="certif" />
+  <div class="progress">
+    <div class="progress-bar" role="progressbar" style="width: 18%;" aria-valuemin="0" aria-valuemax="100">18%</div>
+  </div>
+
+  <div class="espacePersoRowContainer">
+    <div class="card">
+      <div class="card-body">
+        <h2 >
+        <a href="" class="stretched-link">Mes informations personnelles</a>
+        </h2>
+      </div>
     </div>
-    <button type="submit" class="btn btn-primary">Envoyer</button>
-  </form>
+    <div class="card">
+      <div class="card-body">
+        <h2 >
+        <a href="" class="stretched-link">Mon certificat médical</a>
+        </h2>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h2 >
+        <a href="" class="stretched-link">Mes informations personnelles</a>
+        </h2>
+      </div>
+    </div>
+  </div>
+
+
+  <br>
+
+  <div class="formContainer">
+    <form enctype="multipart/form-data" action="index.php?page=EspacePerso" method="post">
+      <div class="mb-3">
+        <input type="hidden" name="MAX_FILE_SIZE" value="300000" />
+        <label for="certif" class="form-label">Certificat médical de moins de 1 an</label>
+        <input type="file" class="form-control" name="certificat" id="certif" />
+      </div>
+      <button type="submit" class="btn btn-primary">Envoyer</button>
+    </form>
+  </div>
 </div>
