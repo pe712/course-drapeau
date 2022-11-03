a22e7ed32752750221f465644ee0e7f70928da1f
pe712 <pe.baviere@gmail.com>
2022-11-03 20:55:37 +0100

    new cards

--- pages/EspacePerso/EspacePersoContent.php
+++ pages/EspacePerso/EspacePersoContent.php
@@ -78,180 +78,196 @@ else
         }
         ?>
     </div>
-</div>
 
-<div>
-    <div class="onglet" id="info">
-        <?php
-        if ($user->prenom == null)
-            echo '<div id="formPerso" class="formContainer">';
-        else {
-        ?>
-            <p class="infosPerso espacePerso-firstLine">Vous avez déjà complété cet onglet.</p>
-
-            <p class="infosPerso">Vous êtes <?php echo $user->prenom . " " . $user->nom . " de la promotion X" . $user->promotion ?></p>
-
-            <button id="modifyPerso" class="btn btn-primary">Modifier mes informations</button>
-            <br><br>
-            <div id="formPerso" class="formContainer" style="display:none">
+    <div>
+        <div class="onglet" id="info">
             <?php
-        }
+            if ($user->prenom == null)
+                echo '<div id="formPerso" class="formContainer">';
+            else {
             ?>
-            <form class="ms-4" method="post" action="index.php?page=EspacePerso">
-                <div class="mb-3">
-                    <label for="firstname" class="form-label">Prénom</label>
-                    <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Eric" required>
-                </div>
-                <div class="mb-3">
-                    <label for="surname" class="form-label">Nom</label>
-                    <input type="text" class="form-control" id="surname" name="surname" placeholder="Labaye" required>
-                </div>
-                <div class="mb-3">
-                    <label for="promo" class="form-label">Promotion X</label>
-                    <input type="number" class="form-control" id="promo" name="promo" value=21 placeholder="21" required>
-                </div>
-                <div id="espacePerso-radio-courreur">
-                    <label class="form-check-label" for="coureur">
-                        Coureur
-                    </label>
-                    <input class="form-check-input" type="radio" name="type" id="coureur" checked>
-                </div>
-                <div id="espacePerso-radio-chauffeur">
-                    <label class="form-check-label" for="chauffeur">
-                        Chauffeur
-                    </label>
-                    <input class="form-check-input" type="radio" name="type" id="chauffeur" value="chauffeur">
-                </div>
-                <div class="mb-3" id="espacePerso-num_places">
-                    <label for="num_places" class="form-label">Nombre de places disponibles <br><span id="espacePerso-form-desc">(4 pour une voiture de 5 par ex)</span></label>
-                    <input type="number" class="form-control" id="num_places" name="num_places" value="4" required>
-                </div>
-                <br>
-                <button type="submit" class="btn btn-primary">Valider</button>
-            </form>
-            </div>
+                <p class="infosPerso espacePerso-firstLine">Vous avez déjà complété cet onglet.</p>
 
-            <button id="retourFromInfo" class="btn btn-primary" onclick="changeView('info', 'cards')">Retour</button>
+                <p class="infosPerso">Vous êtes <?php echo $user->prenom . " " . $user->nom . " de la promotion X" . $user->promotion ?></p>
 
-    </div>
-
-    <div class="onglet" id="certif">
-        <?php
-        if ($user->nom == null) {
-            echo '<p class="espacePerso-firstLine">Vous devez d\'abord remplir la catégorie informations personnelles.</p>';
-        } elseif ($user->chauffeur) {
-            echo '<p class="espacePerso-firstLine">Vous n\'avez pas besoin de renseigner de certificat.</p>';
-        } else {
-            if ($user->certificat) {
-                echo '<div class="formContainer" style="display: none;" id="espacePerso-certificatUpload">';
-            } else {
-                echo '<div class="formContainer" id="espacePerso-certificatUpload">';
+                <button id="modifyPerso" class="btn btn-primary">Modifier mes informations</button>
+                <br><br>
+                <div id="formPerso" class="formContainer" style="display:none">
+                <?php
             }
-        ?>
-            <form enctype="multipart/form-data" action="index.php?page=EspacePerso" method="post">
-                <div class="mb-3">
-                    <input type="hidden" name="MAX_FILE_SIZE" value="500000" />
-                    <label for="certif_uploaded" class="form-label">Certificat médical de moins de 1 an</label>
-                    <input type="file" class="form-control" name="certificat" id="certif_uploaded" />
+                ?>
+                <form class="ms-4" method="post" action="index.php?page=EspacePerso">
+                    <div class="mb-3">
+                        <label for="firstname" class="form-label">Prénom</label>
+                        <input type="text" class="form-control" id="firstname" name="firstname" placeholder="Eric" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="surname" class="form-label">Nom</label>
+                        <input type="text" class="form-control" id="surname" name="surname" placeholder="Labaye" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="promo" class="form-label">Promotion X</label>
+                        <input type="number" class="form-control" id="promo" name="promo" value=21 placeholder="21" required>
+                    </div>
+                    <div id="espacePerso-radio-courreur">
+                        <label class="form-check-label" for="coureur">
+                            Coureur
+                        </label>
+                        <input class="form-check-input" type="radio" name="type" id="coureur" checked>
+                    </div>
+                    <div id="espacePerso-radio-chauffeur">
+                        <label class="form-check-label" for="chauffeur">
+                            Chauffeur
+                        </label>
+                        <input class="form-check-input" type="radio" name="type" id="chauffeur" value="chauffeur">
+                    </div>
+                    <div class="mb-3" id="espacePerso-num_places">
+                        <label for="num_places" class="form-label">Nombre de places disponibles <br><span id="espacePerso-form-desc">(4 pour une voiture de 5 par ex)</span></label>
+                        <input type="number" class="form-control" id="num_places" name="num_places" value="4" required>
+                    </div>
+                    <br>
+                    <button type="submit" class="btn btn-primary">Valider</button>
+                </form>
                 </div>
-                <button id="espacePerso-certif-button" type="submit" class="btn btn-primary">Envoyer</button>
-            </form>
-    </div>
-<?php
-            if ($user->certificat) {
-                $path = $dossier . $name;
-                echo <<<FIN
+
+                <button id="retourFromInfo" class="btn btn-primary" onclick="changeView('info', 'cards')">Retour</button>
+
+        </div>
+
+        <div class="onglet" id="certif">
+            <?php
+            if ($user->nom == null) {
+                echo '<p class="espacePerso-firstLine">Vous devez d\'abord remplir la catégorie informations personnelles.</p>';
+            } elseif ($user->chauffeur) {
+                echo '<p class="espacePerso-firstLine">Vous n\'avez pas besoin de renseigner de certificat.</p>';
+            } else {
+                if ($user->certificat) {
+                    echo '<div class="formContainer" style="display: none;" id="espacePerso-certificatUpload">';
+                } else {
+                    echo '<div class="formContainer" id="espacePerso-certificatUpload">';
+                }
+            ?>
+                <form enctype="multipart/form-data" action="index.php?page=EspacePerso" method="post">
+                    <div class="mb-3">
+                        <input type="hidden" name="MAX_FILE_SIZE" value="500000" />
+                        <label for="certif_uploaded" class="form-label">Certificat médical de moins de 1 an</label>
+                        <input type="file" class="form-control" name="certificat" id="certif_uploaded" />
+                    </div>
+                    <button id="espacePerso-certif-button" type="submit" class="btn btn-primary">Envoyer</button>
+                </form>
+        </div>
+    <?php
+                if ($user->certificat) {
+                    $path = $dossier . $name;
+                    echo <<<FIN
                 <p id="espacePerso-messageCertif" class="espacePerso-firstLine">Vous avez déjà mis votre certificat médical. Cliquez 
                 <a href="" id="espacePerso-download" download><span hidden>$path</span>ici</a> pour le voir et 
                 <a href="" id="espacePerso-modifyCertif">ici</a> pour le modifier</p>
 FIN;
-            }
-        } ?>
+                }
+            } ?>
 
-<button class="btn btn-primary" onclick="changeView('certif', 'cards')" id="espacePerso-retourFromCertif">Retour</button>
+    <button class="btn btn-primary" onclick="changeView('certif', 'cards')" id="espacePerso-retourFromCertif">Retour</button>
 
-</div>
+    </div>
 
-<div class="onglet" id="payement">
-    <?php
-    if ($user->chauffeur)
-        echo '<p>Vous n\'avez pas besoin de payer la course</p>';
-    else
-        echo '<p>Le paiement sera ultérieur</p>';
-    ?>
-    <br>
-    <button class="btn btn-primary" onclick="changeView('payement', 'cards')">Retour</button>
-</div>
-
-<div class="onglet" id="restoration">
-    <form method="post" action="index.php?page=EspacePerso">
+    <div class="onglet" id="payement">
+        <?php
+        if ($user->chauffeur)
+            echo '<p>Vous n\'avez pas besoin de payer la course</p>';
+        else
+            echo '<p class="espacePerso-firstLine">Le paiement sera ultérieur</p>';
+        ?>
         <br>
-        <div class="mb-3">
-            Souhaites-tu manger végétarien ?
-            <span>
-                <label class="form-check-label" for="vege">
-                    Oui
-                </label>
-                <input class="form-check-input" type="radio" name="vegetarian" id="vege" value="vege" checked>
-            </span>
-            <span>
-                <label class="form-check-label" for="not_vege">
-                    Non
-                </label>
-                <input class="form-check-input" type="radio" name="vegetarian" id="not_vege">
-            </span>
-        </div>
-        <div class="mb-3">
-            Veux tu aider à la préparation des repas ?
-            <span>
-                <label class="form-check-label" for="prepa">
-                    Oui
-                </label>
-                <input class="form-check-input" type="radio" name="prepa_repas" id="prepa" value="prepa" checked>
-            </span>
-            <span>
-                <label class="form-check-label" for="not_prepa">
-                    Non
-                </label>
-                <input class="form-check-input" type="radio" name="prepa_repas" id="not_prepa">
-            </span>
-        </div>
-        <div class="mb-3">
-            As-tu des allergies ?
-            <span id="espacePerso-allergie">
-                <label class="form-check-label" for="allergie">
-                    Oui
-                </label>
-                <input class="form-check-input" type="radio" name="has-allergie" id="allergie">
-            </span>
-            <span id="espacePerso-not_allergie">
-                <label class="form-check-label" for="not_allergie">
-                    Non
-                </label>
-                <input class="form-check-input" type="radio" name="has-allergie" id="not_allergie" checked>
-            </span>
-        </div>
-        <div class="mb-3" id="espacePerso-input-allergie">
-            <label for="espacePerso-alergie" class="form-label">Lesquelles?</label>
-            <input type="text" class="form-control" id="espacePerso-alergie" name="allergie">
-        </div>
-        <button type="submit" class="btn btn-primary">Soumettre mes préférences</button>
-    </form>
-    <br>
-    <button class="btn btn-primary" onclick="changeView('restoration', 'cards')">Retour</button>
-</div>
+        <button class="btn btn-primary" onclick="changeView('payement', 'cards')">Retour</button>
+    </div>
 
-<div class="onglet" id="affaires">
+    <div class="onglet" id="restoration">
+        <form method="post" action="index.php?page=EspacePerso">
+            <br>
+            <div class="mb-3">
+                Souhaites-tu manger végétarien ?
+                <span>
+                    <label class="form-check-label" for="vege">
+                        Oui
+                    </label>
+                    <input class="form-check-input" type="radio" name="vegetarian" id="vege" value="vege" checked>
+                </span>
+                <span>
+                    <label class="form-check-label" for="not_vege">
+                        Non
+                    </label>
+                    <input class="form-check-input" type="radio" name="vegetarian" id="not_vege">
+                </span>
+            </div>
+            <div class="mb-3">
+                Veux tu aider à la préparation des repas ?
+                <span>
+                    <label class="form-check-label" for="prepa">
+                        Oui
+                    </label>
+                    <input class="form-check-input" type="radio" name="prepa_repas" id="prepa" value="prepa" checked>
+                </span>
+                <span>
+                    <label class="form-check-label" for="not_prepa">
+                        Non
+                    </label>
+                    <input class="form-check-input" type="radio" name="prepa_repas" id="not_prepa">
+                </span>
+            </div>
+            <div class="mb-3">
+                As-tu des allergies ?
+                <span id="espacePerso-allergie">
+                    <label class="form-check-label" for="allergie">
+                        Oui
+                    </label>
+                    <input class="form-check-input" type="radio" name="has-allergie" id="allergie">
+                </span>
+                <span id="espacePerso-not_allergie">
+                    <label class="form-check-label" for="not_allergie">
+                        Non
+                    </label>
+                    <input class="form-check-input" type="radio" name="has-allergie" id="not_allergie" checked>
+                </span>
+            </div>
+            <div class="mb-3" id="espacePerso-input-allergie">
+                <label for="espacePerso-alergie" class="form-label">Lesquelles?</label>
+                <input type="text" class="form-control" id="espacePerso-alergie" name="allergie">
+            </div>
+            <button type="submit" class="btn btn-primary">Soumettre mes préférences</button>
+        </form>
+        <br>
+        <button class="btn btn-primary" onclick="changeView('restoration', 'cards')">Retour</button>
+    </div>
 
-    <br>
-    <button class="btn btn-primary" onclick="changeView('affaires', 'cards')">Retour</button>
-</div>
+    <div class="onglet" id="affaires">
+    <p class="espacePerso-firstLine">
+            Liste d'affaires à emmener:
+        </p>
+        <ul>
+            <li>Affaires de sport</li>
+            <li>1 montre avec GPS éventuellement (pour suivre l'itinéraire)</li>
+            <li>1 sac de couchage </li>
+            <li>De l'argent au cas où</li>
+            <li>Lampe frontale</li>
+            <li>Sac de running si vous avez</li>
+            <li>Barres de céréales éventuellement</li>
+        </ul>
+        <br>
+        <button class="btn btn-primary" onclick="changeView('affaires', 'cards')">Retour</button>
+    </div>
 
-<div class="onglet" id="hebergement">
-
-    <br>
-    <button class="btn btn-primary" onclick="changeView('hebergement', 'cards')">Retour</button>
-</div>
+    <div class="onglet" id="hebergement">
+        <p class="espacePerso-firstLine">
+            Il y aura deux hébergements sur le parcours pour la totalité de la course:
+        </p>
+        <ul>
+            <li>Courdemanche 72150 (<a href="https://www.google.fr/maps/place/72150+Courdemanche/">voir sur la carte</a>)</li>
+            <li>Magnac-sur-Touvre 16600 (<a href="https://www.google.fr/maps/place/16600+Magnac-sur-Touvre/">voir sur la carte</a>)</li>
+        </ul>
+        <br>
+        <button class="btn btn-primary" onclick="changeView('hebergement', 'cards')">Retour</button>
+    </div>
 </div>
 
 
