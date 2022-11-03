fbee35d3a340fce94233f37724796d5d7d49bad2
pe712 <pe.baviere@gmail.com>
2022-11-03 11:53:55 +0100

    connection conflict of headers sent

--- pages/Unconnect/Unconnect.php
+++ pages/Unconnect/Unconnect.php
@@ -2,7 +2,7 @@
 class Unconnect extends Page
 {
     public function __construct($sections) {
-        session_unset();
+        session_destroy();
         $_SESSION["displayValid"] = "Vous avez correctement été déconnecté.";
         $this->load = "Accueil";
     }
