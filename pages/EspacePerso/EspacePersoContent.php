adae8280920331797f8132b0705ebf16e5e334bc
pe712 <pe.baviere@gmail.com>
2022-11-07 20:20:08 +0100

    ajout à la liste d'affaires à prendre

--- pages/EspacePerso/EspacePersoContent.php
+++ pages/EspacePerso/EspacePersoContent.php
@@ -250,7 +250,8 @@ FIN;
             <li>1 sac de couchage </li>
             <li>De l'argent au cas où</li>
             <li>Lampe frontale</li>
-            <li>Sac de running si vous avez</li>
+            <li>Sac de running et poche à eau si vous avez</li>
+            <li>Bracelet réfléchissant si vous avez (pour la course de nuit)</li>
             <li>Barres de céréales éventuellement</li>
         </ul>
         <br>
