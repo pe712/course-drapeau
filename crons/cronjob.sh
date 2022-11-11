3e5a5fc0b5f82d92a294a1c9d35409afa6e309b4
pe712 <pe.baviere@gmail.com>
2022-11-11 21:42:12 +0100

    cron

--- crons/cronjob.sh
+++ crons/cronjob.sh
@@ -1,2 +1,2 @@
-# Delete the content of the tmp folder if the file  is older than 5 min
-find ../tmp/* -mmin +15 -exec rm {} \;
+# Delete the content of the tmp folder if the file  is older than 15 min
+find ../htdocs/tmp/* -mmin +15 -exec rm {} \;
