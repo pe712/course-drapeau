783aa5e235daad41c7a58122b50207a33087e31b
pe712 <pe.baviere@gmail.com>
2022-11-15 13:48:57 +0100

    paiement

--- htdocs/pages/EspacePerso/cards/payement.php
+++ htdocs/pages/EspacePerso/cards/payement.php
@@ -1,10 +1,9 @@
 <?php
 if ($user->chauffeur)
   echo '<p class="espacePerso-firstLine">Vous n\'avez pas besoin de payer la course</p>';
-else
-  echo '<p class="espacePerso-firstLine">Le paiement sera ultérieur. De l\'ordre de 60€/pers</p>';
-/* 
-if (!$user->paid) {
+// else
+//   echo '<p class="espacePerso-firstLine">Le paiement sera ultérieur. De l\'ordre de 60€/pers</p>';
+else if (!$user->paid) {
   $deb_msg = $sections[1][0];
   $cagnotte_lydia = filter_var($sections[1][1], FILTER_VALIDATE_URL);
   $msg = $sections[1][2];
@@ -23,7 +22,6 @@ if (!$user->paid) {
     Vous avez déjà payé la course.
   </p>
 <?php }
-*/
 ?>
 <br>
 <button class="btn btn-primary" onclick="changeView('payement', 'cards')">Retour</button>
