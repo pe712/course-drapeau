57675a8e4b3eb51a4d4880ad846789d8548216a2
pe712 <pe.baviere@gmail.com>
2022-11-18 19:23:38 +0100

    improving code

--- htdocs/scripts/map.js
+++ htdocs/scripts/map.js
@@ -1,48 +1,48 @@
-if (document.getElementById('map')) {
-    var map1 = L.map('map');
+function add_map(name) {
+    var map = L.map(name);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map1);
-}
-else {
-    var map1 = null
+    }).addTo(map);
+    return map
 }
 
-var map2 = L.map('map2');
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map2);
+if (document.getElementById('map'))
+    var map1 = add_map('map');
+else
+    var map1 = null;
 
+var map2 = add_map('map2')
 var maps = [map1, map2];
 var traces = []
-var parameters = {
-    async: true,
-    marker_options: {
-        startIconUrl: 'lib/leaflet/images/marker-icon.png',
-        endIconUrl: 'lib/leaflet/images/marker-icon.png',
-        shadowUrl: 'lib/leaflet-gpx/pin-shadow.png'
-    }
-}
+
 
 // num est le numéro de la carte (0 pour la carte globale)
 function add_gpx(file, num) {
     var map = maps[num];
-    var url = file; // URL to your GPX file or the GPX itself
     if (num == 0 && !(num in traces)) {
-        traces[num] = new L.GPX(url, parameters).on('loaded', function (e) {
-            map.fitBounds(e.target.getBounds());
-        }).addTo(map);
+        traces[num] = create_gpx(file, map)
     }
     else if (num == 1) {
         if (num in traces)
             traces[num].clearLayers();
-        traces[num] = new L.GPX(url, parameters).on('loaded', function (e) {
-            map.fitBounds(e.target.getBounds());
-        }).addTo(map);
+        traces[num] = create_gpx(file, map);
     }
 }
 
-var marker = new L.Marker([17.385044, 78.486671]);
-marker.addTo(map1);
+function create_gpx(url, map) {
+    var parameters = {
+        async: true,
+        marker_options: {
+            startIconUrl: 'lib/leaflet/images/marker-icon.png',
+            endIconUrl: 'lib/leaflet/images/marker-icon.png',
+            shadowUrl: 'lib/leaflet-gpx/pin-shadow.png'
+        }
+    }
+    var gpx_trace = new L.GPX(url, parameters).on('loaded', function (e) {
+        map.fitBounds(e.target.getBounds());
+    }).addTo(map);
+    return gpx_trace;
+}
+// var marker = new L.Marker([44.83743 - 0.57733]);
+// marker.addTo(map1);
