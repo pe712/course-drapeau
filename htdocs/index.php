1e32e6dfecc2b5e6ac411f853c3d18bc4a55bd8c
pe712 <pe.baviere@gmail.com>
2022-11-18 19:30:39 +0100

    header sent too early

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,11 +1,13 @@
 <?php
-require("pages/includes/headers.php");
+require("pages/include/sessionParam.php");
 session_start();
+require("pages/includes/headers.php");
 
 $files = glob('classes/*.php');
 foreach ($files as $file) {
     require($file);
 }
+
 $conn = Database::connect();
 
 $page_info = PageListing::getCurrent();
@@ -15,15 +17,13 @@ extract($page_info);
 
 <!DOCTYPE html>
 <html lang="fr">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?= $title ?></title>
-        <?php 
-        var_dump($_SESSION);
-        require("pages/includes/linksAndScripts.php") ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <?php require("pages/includes/linksAndScripts.php") ?>
 </head>
 
 <body>
