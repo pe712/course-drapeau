c1088e4479d55ad265bf9988ae4b9ed66aac7f2f
pe712 <pe.baviere@gmail.com>
2022-11-18 23:35:28 +0100

    Mentions

--- htdocs/pages/Mentions/MentionsContent.php
+++ htdocs/pages/Mentions/MentionsContent.php
@@ -1,28 +1,42 @@
 <div class="default-margin espacePerso-firstLine">
-    <p>
+    <div>
         <b>
             Conformément à la Loi pour la Confiance dans l’Économie Numérique (LCEN) de juin 2004, sont indiquées sur cette page les mentions légales obligatoires concernant le site internet course-drapeau.binets.fr.
         </b>
-    </p>
-    <p>
+    </div><br>
+    <div>
         <b>
             ÉDITEUR DU SITE<br>
         </b>
         Course au drapeau - Kès des élèves de l'École polytechnique<br>
         École polytechnique, Route de Saclay, 91120 Palaiseau<br>
-    </p>
-    <p>
+    </div><br>
+    <div>
         <b>
             HÉBERGEUR<br>
         </b>
         Binet Réseau - Kès des élèves de l'École polytechnique<br>
         École polytechnique, Route de Saclay, 91120 Palaiseau<br>
-    </p>
-    <p>
+    </div><br>
+    <div>
         <b>
             DONNÉES<br>
         </b>
-        Nous conservons vos données pour une durée d'au plus 1 ans.<br>
-        Vos données ne sont pas utilisées à des fins commerciales.<br>
-    </p>
+        En vous inscrivant, vous êtes amenés à fournir un certain nombre de données et d’informations notamment lors de votre inscription. Ces données permettent de vous identifier, directement ou indirectement, et peuvent être considérées comme des données personnelles au sens de la réglementation applicable en matière de protection des données personnelles.
+        <br>
+        De façon générale, les données personnelles communiquées par les participants sont destinées uniquement à l'association.
+        <br>
+        Nous utilisons ces données, pour des finalités déterminées, conformément aux dispositions légales applicables et avec votre consentement, et notamment aux fins de :
+        <ul>
+            <li>
+                Permettre la création, la gestion et les accès à votre compte
+            </li>
+            <li>
+                Vous fournir des informations sur l'événement
+            </li>
+            <li>
+                S’assurer du respect des dispositions légales et règlementaires applicables, notamment en matière de non contre-indication médicale
+            </li>
+        </ul>
+    </div>
 </div>
