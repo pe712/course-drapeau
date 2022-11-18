5f1cab34a3b3062473ccf09733ee86f439b845d8
pe712 <pe.baviere@gmail.com>
2022-11-18 19:23:24 +0100

    orthographe

--- htdocs/pages/EspacePerso/cards/hebergement.php
+++ htdocs/pages/EspacePerso/cards/hebergement.php
@@ -15,7 +15,7 @@ $url2 = $baseurl . urlencode($lieu2);
         <a href="<?= $url1 ?>" target="_blank">voir sur la carte</a>
     </div>
     <div>
-        M. et Mme Dezaunay qui nous acceuillent seront là, soyez respectueux.
+        M. et Mme Dezaunay qui nous accueillent seront là, soyez respectueux.
         <br>
         On peut se garer sur un petit parking qui descend de l'autre côté du rond-point ou un peu plus loin sur le bas coté en terre.
         <br>
