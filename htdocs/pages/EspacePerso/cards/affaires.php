26794729ee1392ff4e454d933c18acc2e41e7fb6
pe712 <pe.baviere@gmail.com>
2022-11-18 11:58:57 +0100

    affaires

--- htdocs/pages/EspacePerso/cards/affaires.php
+++ htdocs/pages/EspacePerso/cards/affaires.php
@@ -6,10 +6,9 @@
     <li>1 montre avec GPS éventuellement (pour suivre l'itinéraire)</li>
     <li>1 sac de couchage </li>
     <li>De l'argent au cas où</li>
-    <li>Lampe frontale</li>
+    <li>Lampe frontale avec piles de rechange</li>
     <li>Sac de running et poche à eau si tu as</li>
     <li>Bracelet réfléchissant si tu as (pour la course de nuit)</li>
-    <li>Barres de céréales éventuellement</li>
 </ul>
 <br>
 <button class="btn btn-primary" onclick="changeView('affaires', 'cards')">Retour</button>
