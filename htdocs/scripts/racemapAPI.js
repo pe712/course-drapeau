97adc304b2a377e818c4ada0fd4558272703234c
pe712 <pe.baviere@gmail.com>
2022-11-22 00:39:31 +0100

    APi suivi

--- htdocs/scripts/racemapAPI.js
+++ htdocs/scripts/racemapAPI.js
@@ -1,11 +1,25 @@
 
-
-$.get("https://racemap.com/api/data/v1/6336f41309be310001a5894b/distance", function(data){
-    var balises = data.starters.slice(0,2);
-    var currents = [];
-    balises.forEach(balise => {
-        currents.push(balise.current);
+var real_event_id = "637b302d387e3800013d3044";
+var event_id = "6336f41309be310001a5894b";
+$.get("https://racemap.com/api/data/v1/"+event_id+"/current", function(data){
+    data.starters.forEach(balise => {
+        var current = balise.current;
+        if (current!=null){
+            var lat = current.lat;
+            var lon = current.lng;
+            var distance = current.fromStart;
+            create_marker(lat, lon);
+            update_counter(distance);
+        }
     });
-    console.log(currents)
 })
+
+function create_marker(lat, lon) {
+    console.log(lat, lon);
+  }
+
+  function update_counter(distance){
+    console.log(distance);
+  }
+
 
