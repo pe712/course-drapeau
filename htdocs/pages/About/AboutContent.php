180e315c3c5a52738d49904373d12ef8be9ebcbf
pe712 <pe.baviere@gmail.com>
2022-12-03 18:28:48 +0100

    removing useless complicated class naming

--- htdocs/pages/About/AboutContent.php
+++ htdocs/pages/About/AboutContent.php
@@ -1,33 +1,31 @@
 <?php
 $faq = $sections[1]; //je récupère la première section
 $n = sizeof($faq)/2;
-$numbers = array("Two", "Three", "Four", "Five", "Six", "Seven", "Height", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fiveteen");
 ?>
 
 <section>
     <h2 class="default-margin">Foire Aux Questions</h2>
     <div class="accordion">
         <div class="accordion-item">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse0" aria-expanded="true" aria-controls="collapse0">
                 <?= $faq[0] ?>
             </button>
         </div>
-        <div id="collapseOne" class="accordion-collapse collapse border show" data-bs-parent="#accordionExample">
+        <div id="collapse0" class="accordion-collapse collapse border show" data-bs-parent="#accordionExample">
             <div class="accordion-body">
                 <?= $faq[1] ?>
             </div>
         </div>
         <?php
         for ($k = 1; $k < $n; $k++) {
-            $letter = $numbers[$k];
             $q = $faq[2*$k];
             $r = $faq[2*$k+1];
             echo <<<FIN
             <div class="accordion-item">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse$letter" aria-expanded="false" aria-controls="collapse$letter">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse$k" aria-expanded="false" aria-controls="collapse$k">
                     $q
                 </button>
-                <div id="collapse$letter" class="accordion-collapse collapse border" data-bs-parent="#accordionExample">
+                <div id="collapse$k" class="accordion-collapse collapse border" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
                      $r
                     </div>
