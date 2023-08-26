926860889d0e743fea3b27e7492f87fa8b7488ce
pe712 <pe.baviere@gmail.com>
2023-08-26 14:27:38 +0200

    init django

---
+++ course_drapeau/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CourseDrapeauConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'course_drapeau'
---
+++ docs/docs/setup.md
@@ -0,0 +1,5 @@
+python -m venv .venv
+.venv\Scripts\activate
+pip install -r requirements.txt
+
+If using vscode => run task "run django server"
