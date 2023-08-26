96a45076f8c54f343d67865b446d64b4c8c6a556
pe712 <pe.baviere@gmail.com>
2023-08-26 20:19:51 +0200

    useless cron

--- crons/cronjob.sh
+++
@@ -1,4 +0,0 @@
-# Delete the content of the tmp folder if the file  is older than 15 min
-find ../../www/courseaudrapeau/htdocs/tmp/* -mmin +15 -exec rm {} \;
-
-# */15 * * * * /hosting/www/courseaudrapeau/crons/cronjob.sh
